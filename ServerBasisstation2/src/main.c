ceb4442299648c55e0025249303b67bb658b2520
Sebastian <sebastian.wintgen@informatik.hs-fulda.de>
2024-03-07 19:16:43 +0100

    Fenstersteuerung hinzugefuegt

--- ServerBasisstation2/src/main.c
+++ ServerBasisstation2/src/main.c
@@ -9,23 +9,65 @@
 #include "wifi.h"
 #include "server.h"
 #include "client.h"
+#include "tempHumi.h"
+
+enum fenster
+{
+    zu,
+    auf
+};
 
 void app_main()
 {
+    enum fenster window = auf;
     wifi_init();
     start_webserver();
 
+    // settings for i2c communication with bme680
+    ESP_ERROR_CHECK(i2cdev_init());
+    bme680_init();
+    bme680_config();
+
+    TickType_t last_wakeup = xTaskGetTickCount();
+
     while (true)
     {
+
         if (wifi_established)
         {
             // WiFi Verbindung hergestellt
 
-            printf("Wifi funktioniert\n");
+            startMeasurement();
+            int temp = (int)getTemp();
+            int humi = (int)getHumi();
+            printf("Temperatur: %d °C, Humidität: %d%%\n", temp, humi);
+            if(window == zu)
+            {
+                printf("fenster zu\n");
+            }
+            if(window == auf)
+            {
+                printf("fenster offen\n");
+            }
 
-        } else
+            if ((temp >= 25 || humi >= 80) && window == zu)
+            {
+                printf("Fenster oeffnen wird gesendet\n");
+                char url[] = "http://192.168.110.223/open";
+                urlAufruf(url);
+                window = auf;
+            }
+            if((temp <= 25 && humi <= 80) && window == auf)
+            {
+                printf("Fenster schließen wird gesendet\n");
+                char url[] = "http://192.168.110.223/close";
+                urlAufruf(url);
+                window = zu;
+            }
+        }
+        else
         {
-            printf("Wifi doe not work\n");
+            printf("Keine Verbindung zum Wlan\n");
         }
 
         vTaskDelay(1000 / portTICK_PERIOD_MS); // wait 100 ms
