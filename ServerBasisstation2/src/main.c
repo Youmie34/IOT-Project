a360cbe3b737d885899ba8ce83f1072bcf6c6428
fdai7060 <caroline-elisabeth.schweizer@informatik.hs-fulda.de>
2024-03-09 22:11:46 +0100

    commented, deleted ServerBasisstation, added libs

--- ServerBasisstation2/src/main.c
+++ ServerBasisstation2/src/main.c
@@ -25,27 +25,24 @@ void app_main()
 
     // settings for i2c communication with bme680
     ESP_ERROR_CHECK(i2cdev_init());
+    // initializing and configuration for the bme680
     bme680_init();
     bme680_config();
 
-    TickType_t last_wakeup = xTaskGetTickCount();
-
     while (true)
     {
-
         if (wifi_established)
         {
-            // WiFi Verbindung hergestellt
-
+            // measure temperatur and humidity
             startMeasurement();
             int temp = (int)getTemp();
             int humi = (int)getHumi();
             printf("Temperatur: %d °C, Humidität: %d%%\n", temp, humi);
-            if(window == zu)
+            if (window == zu)
             {
                 printf("fenster zu\n");
             }
-            if(window == auf)
+            if (window == auf)
             {
                 printf("fenster offen\n");
             }
@@ -57,7 +54,7 @@ void app_main()
                 urlAufruf(url);
                 window = auf;
             }
-            if((temp <= 25 && humi <= 80) && window == auf)
+            if ((temp <= 25 && humi <= 80) && window == auf)
             {
                 printf("Fenster schließen wird gesendet\n");
                 char url[] = "http://192.168.110.223/close";
@@ -70,6 +67,6 @@ void app_main()
             printf("Keine Verbindung zum Wlan\n");
         }
 
-        vTaskDelay(1000 / portTICK_PERIOD_MS); // wait 100 ms
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // wait 1s
     }
 }
