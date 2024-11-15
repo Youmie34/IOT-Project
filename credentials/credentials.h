75b3082c8de3bad33b18ca6202a41ff4731a333b
Caro <94118331+Youmie34@users.noreply.github.com>
2024-11-15 01:27:46 +0100

    Update

--- credentials/credentials.h
+++ credentials/credentials.h
@@ -1,6 +1,2 @@
-#define ESP_WIFI_SSID "Pixel_2363"
-#define ESP_WIFI_PASS "2gd7i6ynxa8m4cf"
-/*
-#define ESP_WIFI_SSID "Pixel"
-#define ESP_WIFI_PASS "CaroNet4"
-*/
+#define ESP_WIFI_SSID
+#define ESP_WIFI_PASS
